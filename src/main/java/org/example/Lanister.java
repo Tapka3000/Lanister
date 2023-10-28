cfdcb9c8dc6891ecc1a5bca36ca3bea2dd859e4a
Tapka3000 <circle.table@yandex.ru>
2023-10-28 19:51:07 +0300

    Commit

--- src/main/java/org/example/Lanister.java
+++ src/main/java/org/example/Lanister.java
@@ -1,2 +1,13 @@
-package org.example;public class Lanister {
+package org.example;
+
+public class Lanister {
+    public static void main(String[] args) {
+        int infantry = 1500;
+        int orcs = 1400;
+        int armies = infantry + orcs;
+
+        System.out.println("Количество войск Ланистеров " + infantry);
+        System.out.println("Количество войс у Орков " + orcs);
+        System.out.println("Общее количество войск " + armies);
+    }
 }
